1a361b1575e2b6a14e6498337b7b70345c37e9ba
Deva Kumar <deva.kumar@sas.com>
2020-01-22 03:35:53 -0500

    minor fixes

--- packages/examples/runcaslasAction.js
+++ packages/examples/runcaslasAction.js
@@ -1,48 +1,55 @@
-"use strict";
+/*
+* Copyright © 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+* SPDX-License-Identifier: Apache-2.0
+*/
 
 let restaf = require('@sassoftware/restaf');
 let payload = require('./config')();
-let {casSetup, print} = require('@sassoftware/restaflib');
+let restaflib = require('@sassoftware/restaflib');
+let {print} = restaflib;
 
 let store = restaf.initStore();
-async function example () {
-  let {session} = await casSetup (store, payload);
-  // console.log(JSON.stringify(session.links(), null, 4));
-  let casl = `
-  cols = {'char', 'char','double', 'REASON', 'JOB','CLAGE'};
 
-  chars = {};
+async function test_runAction () {
+    let {session} = await restaflib.casSetup (store, payload);
+    /* user code */
+    let casl = `
+    cols = {'char', 'char','double', 'REASON', 'JOB','CLAGE'};
 
-  nums = {};
-              length_of_each = dim(cols)/2;
-              do i = 1 to length_of_each;
-                 if (cols[i] == 'char' or cols[i] == 'varchar') then do;
-                    chars = chars + cols[i+length_of_each];
-                  end;
-                 else if (cols[i] == 'double')  then do;
-                     nums = nums + cols[i+length_of_each];
-                  end;
-              end;
-              full_dict = nums + chars;
+    chars = {};
 
-              send_response(full_dict);
+    nums = {};
+                length_of_each = dim(cols)/2;
+                do i = 1 to length_of_each;
+                   if (cols[i] == 'char' or cols[i] == 'varchar') then do;
+                      chars = chars + cols[i+length_of_each];
+                    end;
+                   else if (cols[i] == 'double')  then do;
+                       nums = nums + cols[i+length_of_each];
+                    end;
+                end;
+                full_dict = nums + chars;
 
+                send_response(full_dict);
+          `;
+   let p = {
+     action: 'sccasl.runCasl',
+     data  : {code: casl}
+   };
 
-        `;
+  
+    let r = await store.runAction (session, p);
+    let result = r.items().toJS();
 
-  let p = {
-    action: "sccasl.runcasl",
-    data  : { code: casl }
-  };
-
-  let r = await store.runAction(session, p);
-  debugger;
-
-  print.items(r, 'Cas Results');
- 
-  return "done";
-}
-
-example()
-  .then(r => console.log(r))
-  .catch(err => console.log(err));
+    // print.object(result, 'Cas Results');
+    console.log(`Job status: ${result.status}`);
+    print.object(result.log, 'Log');
+    print.object(result.disposition, 'Disposition');
+    print.object(result.results, 'Data of real interest');
+    await store.apiCall(session.links('delete'));
+    return 'done';
+   }
+  
+   test_runAction()
+    .then (r => console.log(r))
+    .catch(err => console.log(err));

bf86a71f0bfbe9344abba6652e189ae0fcfd125e
Deva Kumar <deva.kumar@sas.com>
2020-05-31 12:40:04 -0400

    save

--- packages/viyacaddy/src/rimport.js
+++ packages/viyacaddy/src/rimport.js
@@ -1,11 +1,11 @@
 /*
-* Copyright © 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
-* SPDX-License-Identifier: Apache-2.0
-*/
+ * Copyright © 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 'use strict';
 const fs = require('fs').promises;
 
-module.exports = async function rimport (store, reports, args) {
+module.exports = async function rimport(store, reports, args) {
 	let { file, name, uri } = args.options;
 	// delete old version of report
 	let p = {
@@ -24,22 +24,25 @@ module.exports = async function rimport (store, reports, args) {
 			parentFolderUri: uri,
 		},
 		data: {
-			name       : name,
+			name: name,
 			description: name,
 		},
 	};
 	let newReport = await store.apiCall(reports.links('createReport'), payload);
 
 	// read file and update the content of the new report
-	let savedReport = await fs.readFile(file);
-	let type = 'json';
-	if (file.search('.xml') >= 0) {
-		type = 'xml';
+
+	let type = file.split('.').pop().toLowerCase();
+	if (!(type === 'json' || type === 'xml')) {
+		throw 'File extension must be json or xml';
 	}
+
+	let savedReport = await fs.readFile(file);
+
 	payload = {
 		headers: {
 			'Content-Type': `application/vnd.sas.report.content+${type}`,
-			'Accept'      : '*/*',
+			'Accept': '*/*',
 		},
 		data: `${savedReport}`,
 	};

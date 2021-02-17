a6e7b0452537c882872f7d22de5965d662d59287
Deva Kumar <deva.kumar@sas.com>
2021-02-17 11:52:28 -0500

    using webpack 4.0

--- packages/restaflib/webpack.config.js
+++ packages/restaflib/webpack.config.js
@@ -1,74 +1,72 @@
 'use strict' ;
 
-// let nodeExternals  = require('webpack-node-externals');
+let nodeExternals  = require('webpack-node-externals');
 let webpack = require('webpack');
 let  path    = require('path');
 let library = 'restaflib';
-let argv    = require('yargs').argv;
-let prod    = (argv.p === true) ;
-let env     =  argv.env;
-let asNode  = false;
-if (env != null) {
-    asNode = (env.node === true);
-}
 
-let APP_PATH = path.resolve(__dirname, 'src');
-let plugins = [];
-let outputFile;
+module.exports = (env) => {
+    let APP_PATH = path.resolve(__dirname, 'src');
+    let plugins = [];
+    let outputFile;
 
-    console.log(` ProductionMode: ${prod} `);
-    console.log(` nodejs module : ${asNode}`);
+        console.log(` production build: ${env.p} `);
+        console.log(` target          : ${env.target}`);
 
-    outputFile = (argv.p === true) ? library + '.min.js' : library + '.js';
+        outputFile = (env.p === 'y') ? library + '.min.js' : library + '.js';
 
-    if (asNode === false) {
-        plugins.push(new webpack.DefinePlugin({
-            'process.env.NODE_ENV': '"production"',
-            'global'              : {}
-        }));
-    }
-
-    
-    let config = {
-        context: APP_PATH,
-        mode   : (prod === true) ? 'production' : 'development',
-        entry  : [
-            APP_PATH + '/index'
-         ],
-
-        output: {
-            path          : (asNode === true) ? path.join(__dirname, 'lib') : path.join(__dirname, 'dist'),
-            library       : library,
-            libraryTarget : 'umd',
-            filename      : outputFile,
-            umdNamedDefine: true
-        },
-        node: {
-            fs: 'empty'
-        },
-
-        module: {
-            rules: [
-                {
-                    test   : /\.(js|jsx)$/,
-                    use    : [{ loader: "babel-loader" }],
-                    include: APP_PATH,
-                    exclude: /node_modules/
-                }
-            ]
-        },
-
-        plugins: plugins,
-
-        resolve: {
-            extensions: ['.js']
+        if (env.p === 'y') {
+            plugins.push(new webpack.DefinePlugin({
+                'process.env.NODE_ENV': '"production"',
+                'global'              : {}
+            }));
         }
-    };
 
-    if (asNode === true) {
-       // config.externals = [ nodeExternals() ];
-        config.target    =  'node';
+        
+        let config = {
+            context: APP_PATH,
+            mode   : (env.p === 'y') ? 'production' : 'development',
+            optimization: {
+                usedExports: true
+            },
+            entry  : [
+                APP_PATH + '/index'
+            ],
+
+            output: {
+                path          : (env.target === 'node') ? path.join(__dirname, 'lib') : path.join(__dirname, 'dist'),
+                library       : library,
+                libraryTarget : 'umd',
+                filename      : outputFile,
+                umdNamedDefine: true
+            },
+            node: {
+                fs: 'empty'
+            },
+
+            module: {
+                rules: [
+                    {
+                        test   : /\.(js|jsx)$/,
+                        use    : [{ loader: "babel-loader" }],
+                        include: APP_PATH,
+                        exclude: /node_modules/
+                    }
+                ]
+            },
+
+            plugins: plugins,
+
+            resolve: {
+                extensions: ['.js']
+            }
+        };
+
+        if (env.target  === true) {
+            config.externals = [nodeExternals()];
+            config.target    =  'node';
+        }
+        return config;
     }
 
-module.exports = config;
 

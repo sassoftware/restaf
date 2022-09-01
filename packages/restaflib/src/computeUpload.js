7a9e83bfa0df52fb0076f3d3e870a5e06b23e9e0
devakumaraswamy <deva.kumar@sas.com>
2022-09-01 17:38:34 -0400

    save

--- packages/restaflib/src/computeUpload.js
+++ packages/restaflib/src/computeUpload.js
@@ -12,19 +12,15 @@ let fs = require('fs').promises;
  * @async
  * @module computeUpload
  * @param {store} store 
- * @param {rafobject} columns compute session 
- * @param {object} columns 
- * @param {computeTable} output 
- * @param {array} csv   an array of comma seperated strings  
+ * @param {rafobject} session compute session 
+ * @param {computeTable} table - name of table to be created
+ * @param {object} data - rows of data objects
+ * @param {object} columns columns
  * @returns {promise}
  */
-async function computeUpload (store, session, columns, output, altSrc){
-    // parse source file to figure out what we are uploading
-    let table = output;
-    if (typeof output === 'object'){
-       table = `${output.caslib}.${output.name}`;
-    };
-
-    return {msg: "To be implemented", statusCode: 0};
-}
+async function computeUpload (store, session, table, data, columns){
+   // move the code from restafedit after further testing...
+   return {msg: 'Placeholder', statusCode: 0};
+  }
+ 
 export default computeUpload;
